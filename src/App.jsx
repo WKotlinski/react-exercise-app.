8dd8acd013906586ad0b359413e2824be46a1188
WKotlinski <132941585+WKotlinski@users.noreply.github.com>
2024-02-13 10:40:51 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Input from "./components/Input/Input";
+import ListOfExercises from "./components/ListOfExercises/ListOfExercises";
+
+function App() {
+  return (
+    <>
+      <App />
+    </>
+  );
+}
+
+export default App;
